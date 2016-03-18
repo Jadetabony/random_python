d1b6afb3070f50c1b538531708a780258233077e
Jadetabony <jat257@cornell.edu>
2016-03-17 23:13:44 -0700

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -5,17 +5,20 @@ Blackjack player
 @author: Jade
 """
 
-# create deck of cards 
 import random
 
+#set score to 0-0 at the beginning
 score_player = 0
 score_dealer = 0
 
+
 while True:
+    #print score at the beginning of each round
     print "Score- "
     print "Dealer: ", score_dealer
     print "You: ", score_player
-        
+    
+    #create a deck of cards
     suits = ['D', 'C', 'H', 'S']
     ranks = ['2','3','4','5','6','7','8','9','T','J','Q','K','A']
     
@@ -27,10 +30,11 @@ while True:
     
     
     
-    # deal hand
+    
     #Shuffle deck
     random.shuffle(deck)
     
+    # deal hand
     dealer_bottom = deck[0]
     deck.remove(dealer_bottom)
     
@@ -57,48 +61,31 @@ while True:
         else:
             return int(rk)
      
-    # Define function to print out the written rank and suit
-    """def written(card):
-        write =[]
-        rk = card[0]
-        if rk == 1:
-            write[0]= 10
-        elif rk =='J':
-            write[0]= 'Jack'
-        elif rk == 'Q':
-            write[0] = 'Queen'
-        elif rk =='K':
-            write[0]= 'King'
-        elif rk == 'A':
-            write[0] = 'Ace'
-        else:
-            write[0] = int(rk)
-            
-        suit= card[1]
-        if suit == 'H':
-            write[1]= 'Hearts'
-        elif suit =='D':
-            write[0]= 'Diamonds'
-        elif suit == 'C':
-            write[0] = 'Clubs'
-        elif suit =='S':
-            write[0]= 'Spades' 
-            
-        return (write[0], " of ", write[1])
-        """
-    
+
     
     # print visible dealer and player hands
     print ("You have a %s visible and a %s." %(player_top, player_bottom))
     print ("The dealer has a %s visible." %(dealer_top))
     
-    #
+    #create a custom sort function for the card rank order to be used in the score function
+    def cmp_sort(a,b):
+        ranks = ['2','3','4','5','6','7','8','9','T','J','Q','K','A']
+        if ranks.index(a[0]) > ranks.index(b[0]):
+            return 1
+        elif ranks.index(a[0]) == ranks.index(b[0]):
+            return 0
+        else:
+            return -1
+    
+
+    # Function to calculate score.  In order to deal with the fact that A can be a 1 or 11, cards are ordered using the custom rank,
+    # and then the hand is totaled, dealing with A last.  If total is greater than 10 by the time that you arrive at the A, A == 1. 
+    # Else, A == 11
     def score(hand):
         
         total = 0
         #sort cards so that A will always be last
-        """ranks = ['2','3','4','5','6','7','8','9','T','J','Q','K','A']
-        sorted(hand, key= ranks)"""
+        hand.sort(cmp_sort)
         for card in hand:
             if total > 10 and card[0] == 'A':
                 total = total + 1
@@ -107,9 +94,10 @@ while True:
         return total
         
     
-    # hit vs stay option
+    # Loop that seeks user input on whether then want to hit or stay and then calculates their score, determines if they got blackjack 
+    # or if they bust
     while True:
-        if score(player_hand)== 21:
+        if score(player_hand)== 21 and len(player_hand) == 2:
             print ("BLACKJACK!")
             break
         else:
@@ -135,9 +123,9 @@ while True:
     
     # Determine dealer score
     while True:
-        if score(player_hand) > 21:
+        if score(player_hand) > 21 :
             break
-        elif score(dealer_hand) == 21:
+        elif score(dealer_hand) == 21 and len(dealer_hand) == 2:
             print "Dealer got Blackjack."
             break
         elif score(dealer_hand) <= 16:
@@ -178,9 +166,16 @@ while True:
         score_player += 1
     else:
         print "Error???"
-
-    
-    cont = raw_input("Would you like to play again? [y/n]: ")
+        
+    # seeks user input on whether or not they would like to play again, with a loop to ensure that a y or n was entered
+    while True:
+        cont = raw_input("Would you like to play again? [y/n]: ")
+        if cont not in ['y','n','Y','N']:
+            print "Please enter only a 'y' or 'n'."
+            continue
+        else:
+            break 
+        
     if cont == 'y':
         continue
     else:
